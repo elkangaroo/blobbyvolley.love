aba96d6a89373ac03c7743a8e20d749e271a143e
Alexander Wenzel <elkangaroo@googlemail.com>
2020-01-19 00:13:02 +0100

    add löve skeleton

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+  t.version = "11.3"
+  t.console = false
+  t.window.title = "I löve Blobby Volley"
+  t.window.width = 800
+  t.window.height = 600
+end
---
+++ main.lua
@@ -0,0 +1,30 @@
+local app = {}
+app.version = 0.1
+
+function love.load(...)
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.focus(focused)
+
+end
+
+function love.quit()
+
+end
+
+function love.keypressed(key, scancode, isrepeat)
+
+end
+
+function love.keyreleased(key, scancode)
+
+end
